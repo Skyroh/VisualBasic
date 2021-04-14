bcb781bc05a7633bcb3035a1b70cda782e32f6f7
Skyroh <57754814+Skyroh@users.noreply.github.com>
2021-04-14 19:29:15 +0200

    Add files via upload

---
+++ HolaMundo/HolaMundo/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim mensaje As String                   'DECLARAMOS VARIABLE MENSAJE COMO STRING'
+        mensaje = "Hola Mundo"                  'DEFINIMOS EL VALOR DE MENSAJE'
+        MsgBox(mensaje)                         'iNCOCAMOS UNA VENTANA CON UN MENSAJE'
+    End Sub
+End Class
